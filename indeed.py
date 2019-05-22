166d1dc884c0adbdfa98b50f7d4e67f73dff1d76
Jin Young Bang <jybang@bu.edu>
2019-05-22 14:51:03 +0800

    Added user input function

--- indeed.py
+++ indeed.py
@@ -4,9 +4,19 @@ import random
 import sys
 from bs4 import BeautifulSoup
 
-#### LinkedIn Software Engineering Intern Job Board Parser ####
+#### Indeed Job Board Parser ####
 
-URL = "https://www.indeed.com/jobs?q=software+engineering+intern&l="
+# User enters their information and assigns it as variables
+position = input("What positions are you looking for? \n")
+location  = input("In which area? \n")
+print()
+
+# Reformats data into URL's syntax
+URL = "https://www.indeed.com/jobs?q=" + position.replace(" ", "+") +"&l=" + location.replace(" ", "+")
+print("Here is the Link to your search term: \n" + URL)
+print()
+
+# Utilizes BeautifulSoup Library to Parse Indeed.com
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib')
 
@@ -16,43 +26,14 @@ loadingAgain = False
 overkill = 0
 counter = 5
 
-## While loop algorithm doesn't work :(
-# while companyName == []:
-#     companyName = soup.find_all('h4', attrs={"class": "result-card__subtitle"}, limit=10)
-#     positionName = soup.find_all('a', attrs={"class": "result-card__full-card-link"}, limit=10)
-#     linkName = soup.find_all('a', attrs={"class": "result-card__full-card-link"}, href=True, limit=10)
-
-#     print(companyName)
-
-#     if companyName == []:
-
-#         if loadingAgain is False:
-#             print("Loading... (Please wait!)")
-#             time.sleep(counter)
-#             loadingAgain = True
-
-#         else:
-#             if overkill < 5:
-#                 print("Loading... (Just a bit more!)")
-#             else:
-#                 print("Loading... (It's getting there!)")
-
-#             if overkill == 10:
-#                 print()
-#                 print("FAILED: Try run the program again later :(")
-#                 break
-
-#             time.sleep(counter)
-#         counter += random.randint(1, 5)
-#         overkill += 1
-
 ### MAXIMUM LIMIT: 18 ###
 companyName = soup.find_all('span', attrs={"class": "company"}, limit=18)
 positionName = soup.find_all('div', attrs={"class": "title"}, limit=18)
 linkName = soup.find_all('a', attrs={"class": "jobtitle"}, href=True, limit=18)
 
+# If program is unable to parse any data from the website, program will exit.
 if companyName == []:
-    print("URL Request Overload: Please run this program again in a few seconds!")
+    print("Please run this program again in a few seconds!")
     sys.exit()
 
 # Data sets
